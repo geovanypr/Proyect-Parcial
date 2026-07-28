035f54c11cbc84927cf57330e939057f61892184
O11CE <o11ce@example.com>
2026-07-28 19:56:26 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>ConvertX</h1>
+      </header>
+      <main className="main-content">
+      </main>
+    </div>
+  )
+}
+
+export default App
